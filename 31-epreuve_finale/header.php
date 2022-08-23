6503597e29d6b7b2b838b23b903274cc55b6fe7f
MarcoU <97858294+MarcoU2021@users.noreply.github.com>
2022-08-23 17:37:33 -0400

    Premièr commit

---
+++ 31-epreuve_finale/header.php
@@ -0,0 +1,47 @@
+<?php
+ /**
+  * Template header
+  * Ve template est intégré dans tous les modèles de thème
+  * @link : https://developer.wordpress.org/themes/
+  * @package : 31w
+  */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>  >
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body>
+    <section class="site">
+        <header class="site__header">
+            <section class="site__branding">
+                <h1>
+                    <a href="<?= esc_url( home_url( '/' ) ) ?>"><?= bloginfo( 'name' ); ?></a>
+                </h1>
+                <h2>
+                <?= bloginfo( 'description' ); ?>
+                </h2>
+            </section>
+            <nav class="site__navigation">
+            Menu
+            </nav>
+        </header>
+        <section class="site__sidebar">
+
+
+            <input type="checkbox" name="chk-burger" id="chk-burger">
+            <label for="chk-burger" id="burger">
+                <img src="https://s2.svgbox.net/hero-solid.svg?ic=menu&color=000000" width="32" height="32">
+            </label>
+            <?php wp_nav_menu(array(
+                        'menu' => 'menu sidebar',
+                        'container' => 'nav',
+                        'container_class' => 'menu__sidebar',
+                        'menu_class' => 'menu__sidebar__ul'
+            )) ?>
+        </section>
+    
+
---
+++ 31-epreuve_finale/readme.md
@@ -0,0 +1,13 @@
+# TP2
+## Thème et plugin  Wordpress
+
+
+[github-page](https://MarcoU2021.github.io/31w)
+> Le thème utilise une structure de grid pour formater le layout
+
+
+Pour plus d'information sur la conception de thème
+[WP developper guide](https://developper.wordpress.org/theme)
+
+Pour plus d'information sur la structure grid
+[css-tricks/grid](https://css-tricks.com/snippets/css/complete-guide-grid/)
