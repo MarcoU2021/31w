006c5d23d4b00c4743886cf6f27d30f6bfa6b1bd
MarcoU <97858294+MarcoU2021@users.noreply.github.com>
2022-08-24 22:15:03 -0400

    Add files via upload
    add Titre - side__main h1 College Maisonneuve

---
+++ single.php
@@ -0,0 +1,21 @@
+<?php get_header(); ?> 
+<section class="site__main">
+<h1>Collège Maisonneuve</h1>
+<?php
+if (have_posts()):
+    while(have_posts()) : the_post(); ?>
+        <h2><?php the_title(); ?></h2>
+      <p><?php the_content(); ?></p>
+  <?php
+    $resume = get_field("description");
+    echo "<p>la description: " . $resume . "</p>";
+    $image = get_field('image');
+      if( !empty( $image ) ): ?>
+          <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+      <?php endif; ?>
+
+
+    <?php endwhile; ?>
+<?php endif; ?>    
+</section>
+<?php get_footer(); ?>
