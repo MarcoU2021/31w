8231ddd374b98a42257323d190ad68f2aac5b8a2
MarcoU <97858294+MarcoU2021@users.noreply.github.com>
2022-08-24 22:13:05 -0400

    Add files via upload
    change titre College Maisonneuve

---
+++ front-page.php
@@ -0,0 +1,26 @@
+<?php get_header(); ?> 
+<section class="site__main">
+    <h1>College Maisonneuve</h1>
+    <div class="blocs__horizontaux">
+    <?php
+        if (have_posts()):
+            while(have_posts()) : the_post(); ?>
+            <article>
+                <h5><a href="<?= get_permalink() ?>"><?php the_field('titre'); ?></a></h2>
+                <h3><?php the_field('sous_titre'); ?></h3>
+                <p><?php the_field('resume'); ?></p>
+            </article>
+            <?php endwhile; ?>
+        <?php endif; ?>   
+    </div>
+    <?php wp_nav_menu(array(
+                'menu' => 'menu accueil',
+                'container' => 'nav'
+
+            ));
+            ?>
+
+</section>
+
+
+<?php get_footer(); ?>
